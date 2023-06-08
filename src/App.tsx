e90a0f30f04336f039694da57665ef787780585f
NestorSanchez <nesaenz.69@gmail.com>
2023-06-08 18:40:07 -0500

    :sparkles: create state and interface type todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,47 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+interface TodoType {
+  id: number;
+  text: string;
+  date: string;
+  isDone: boolean;
+}
+  
+const [todoList, setTodoList] = useState<Array<TodoType>>([
+  {
+    id: 1,
+    text: "Lo que sea mi paisano",
+    date: new Date().toLocaleDateString(),
+    isDone: false,
+  },
+  {
+    id: 2,
+    text: "Pa las que sea",
+    date: new Date().toLocaleDateString(),
+    isDone: false,
+  },
+  {
+    id: 3,
+    text: "Como fué como fué",
+    date: new Date().toLocaleDateString(),
+    isDone: false,
+  }
+])
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ul>
+        {
+          todoList.map((todo) => (
+            <li key={todo.id}>
+              <h3>{todo.text}</h3> 
+              <p>{todo.date}</p>
+            </li>
+           ))
+        }
+      </ul>
     </>
   )
 }
